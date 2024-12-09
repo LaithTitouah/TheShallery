e233bd3e671ee7c0b94ee087ed9dfc33768a9743
JMartLMU <jmart253@lion.lmu.edu>
2024-12-08 17:25:49 -0800

    Other User by ID

--- src/components/ViewFavorites.jsx
+++ src/components/ViewFavorites.jsx
@@ -1,42 +1,12 @@
 import { useState } from "react";
 import { getMyFavorites } from "../services/favoriteService";
 import { loggedInUserId } from "../services/authService";
-import { deleteFavorite } from "../services/favoriteService";
+import Display from "./DisplayFavorites";
 
 export default function ViewFavorites({ user }) {
     const [favorites, setFavorites] = useState([]);
     const [displayVisible, setDisplayVisibility] = useState(false);
     const [removeVisable, setRemoveVisibility] = useState(false);
-
-    function DisplayFavorites() {
-        return displayVisible && (
-            <>
-                <div>
-                <button onClick={() => setDisplayVisibility(false)}>Close My Ratings</button>
-                <h2>----------------My Favorites----------------</h2>
-                {favorites.length > 0 ? (
-                    
-                    favorites.map((fav) => (
-                    <div key={fav.id}>
-                        <img src={fav.image} alt={fav.name} />
-                        <p>Rating: {fav.score}/10</p>
-                        <h3>{fav.name}</h3>
-                        <p dangerouslySetInnerHTML={{ __html: fav.summary }}></p>
-                        <p>
-                            {removeVisable ?
-                            <button onClick={() => Delete(fav.id)}>Remove Entry Above</button> : ""
-                            }
-                        </p>
-                    </div>
-                    ))
-                ) : (
-                    <p>No favorites yet!</p>
-                )}
-                <button onClick={() => setDisplayVisibility(false)}>Close My Ratings</button>
-                </div>
-            </>
-            );
-    }
 
     async function UpdateFavorites() {
         async function fetchFavorites() {
@@ -49,27 +19,23 @@ export default function ViewFavorites({ user }) {
         if (user.uid == loggedInUserId()) {
             console.log("This is my page")
             setRemoveVisibility(true);
-        } else {
-            console.log("This is NOT my page")
-            setRemoveVisibility(true);
         };
-        setDisplayVisibility(true);
+        setDisplayVisibility((prev) => !prev);
 
         return console.log("Favorites Updated");
     }; 
-
-    async function Delete(id) {
-        // console.log("Deleting favorite with id:", id);
-        deleteFavorite({showId:id});
-        UpdateFavorites();
-    }
 
     return (
         <div>
             {user ? (
                 <div>
                     <button onClick={UpdateFavorites}>View My Ratings</button>
-                    <DisplayFavorites />
+                    <Display 
+                    favorites={favorites} 
+                    displayVisible={displayVisible} 
+                    removeVisable={removeVisable}
+                    setDisplayVisibility={setDisplayVisibility}
+                    />
                 </div>
             ) : (
                 <></>
